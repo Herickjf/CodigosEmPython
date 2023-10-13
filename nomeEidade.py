456cb57c6093bb31d3529fa1323c278b26e007fd
Herickjf <125290993+Herickjf@users.noreply.github.com>
2023-10-12 21:58:44 -0300

    Add files via upload

---
+++ nomeEidade.py
@@ -0,0 +1,5 @@
+nome = str(input("Digite seu nome:  "))
+idade = int(input("\nDigite sua idade: "))
+print("\nNome: ", nome, "\nIdade: ", idade)
+idade /=2
+print("\n\nPrimeira letra do nome: ", nome[0], "\nÚltima letra do nome: ", nome[13], "\nMetade da idade: ", idade)
---
+++ tabuada.py
@@ -0,0 +1,6 @@
+n = int(input("Digite um número para receber a tabuada dele: "))
+i = 1
+while i<=10:
+  print("\n", n, " x ", i, " = ", n*i)
+  i+=1
+print("\nfeito.")
