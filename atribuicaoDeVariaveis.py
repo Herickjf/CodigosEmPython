248ef53fdf2c29a72cc2bd7b603275c5e7dca214
Herickjf <125290993+Herickjf@users.noreply.github.com>
2023-11-18 13:34:28 -0300

    Update and rename atribuicaoDeVariaveis.py to basico/atribuicaoDeVariaveis.py

--- atribuicaoDeVariaveis.py
+++
@@ -1,8 +0,0 @@
-'''Passar o suco de um copo para o outro sem "misturar".'''
-copo1 = "laranja"
-copo2 = "acerola"
-suco1 = copo2
-suco2 = copo1
-copo1 = suco1
-copo2 = suco2
-print(f"copo1: {copo1}\ncopo2: {copo2}")
---
+++ basico/atribuicaoDeVariaveis.py
@@ -0,0 +1,8 @@
+'''Passar o suco de um copo para o outro sem "misturar" os sucos.'''
+copo1 = "laranja"
+copo2 = "acerola"
+suco1 = copo2
+suco2 = copo1
+copo1 = suco1
+copo2 = suco2
+print(f"copo1: {copo1}\ncopo2: {copo2}")
