4cd5335c24eeaa7cf71f1d8fbe0645e245ab5e82
Herickjf <125290993+Herickjf@users.noreply.github.com>
2023-11-18 13:40:50 -0300

    Update and rename nomeEidade.py to basico/nomeEidade.py

--- basico/nomeEidade.py
+++ basico/nomeEidade.py
@@ -2,4 +2,4 @@ nome = str(input("Digite seu nome:  "))
 idade = int(input("\nDigite sua idade: "))
 print("\nNome: ", nome, "\nIdade: ", idade)
 idade /=2
-print("\n\nPrimeira letra do nome: ", nome[0], "\nÚltima letra do nome: ", nome[13], "\nMetade da idade: ", idade)
+print("\n\nPrimeira letra do nome: ", nome[0], "\nMetade da idade: ", idade)
