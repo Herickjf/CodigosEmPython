1eba0d850f346ba2c46a638578491fe662e9202f
Herickjf <125290993+Herickjf@users.noreply.github.com>
2023-11-18 13:35:35 -0300

    Update and rename conversaoDeVelocidade.py to basico/conversaoDeVelocidade.py

---
+++ basico/conversaoDeVelocidade.py
@@ -0,0 +1,2 @@
+vel = float(input("Digite a velocidade (m/s): "))
+print(f"Essa velocidade equivale a {vel*3.6:.1f} km/h.")
--- conversaoDeVelocidade.py
+++
@@ -1,2 +0,0 @@
-vel = float(input("Digite a velocidade (m/s): "))
-print(f"Essa velocidade equivale a {vel*3.6:.1f} km/h.")
