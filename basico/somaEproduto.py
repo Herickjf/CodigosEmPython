0a9508ce36001d4bc0622cc2e3eff9a80bcc9cb4
Herickjf <125290993+Herickjf@users.noreply.github.com>
2023-11-18 13:42:21 -0300

    Update and rename somaEproduto.py to basico/somaEproduto.py

---
+++ basico/somaEproduto.py
@@ -0,0 +1,3 @@
+valor1 = float(input("Digite um valor: "))
+valor2 = float(input("Digite outro valor: "))
+print(f"\n{valor1} + {valor2} = {valor1 + valor2:.1f}\n{valor1} x {valor2} = {valor1 * valor2:.1f}")
